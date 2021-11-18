6252231eba6296bc3989dac564c0a2d4d8bfe59a
Maxi <maximilian.lincks@gmx.net>
2021-11-18 11:37:38 +0100

    fixed security settings

--- src/main/java/com/lecturefeed/authentication/AdminSecurityConfigAdapter.java
+++ src/main/java/com/lecturefeed/authentication/AdminSecurityConfigAdapter.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @Configuration
@@ -12,12 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class AdminSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
-                .and().authorizeRequests()
-                .antMatchers("/auth/admin or /session/create")
-                .access("hasIpAddress(\"127.0.0.1\") or hasIpAddress(\"::1\")")
-                .anyRequest().permitAll()
-                .and().csrf().disable();
+        http
+            .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
+            .authorizeRequests()
+            .antMatchers("/auth/admin", "/session/create")
+            .access("hasIpAddress('127.0.0.1') or hasIpAddress('::1')")
+            .anyRequest().authenticated();
     }
 }
 
--- src/main/java/com/lecturefeed/model/QuestionModel.java
+++ src/main/java/com/lecturefeed/model/QuestionModel.java
@@ -2,8 +2,6 @@ package com.lecturefeed.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
