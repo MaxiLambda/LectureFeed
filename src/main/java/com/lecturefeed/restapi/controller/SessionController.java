02934a27735fc93e1eaa7778c5226ea7b96205d8
viktor.aigenseer@web.de <viktor.aigenseer@gmail.com>
2021-12-02 11:45:38 +0100

    fixes on reviews

--- src/main/java/com/lecturefeed/restapi/controller/SessionController.java
+++ src/main/java/com/lecturefeed/restapi/controller/SessionController.java
@@ -7,8 +7,6 @@ import com.lecturefeed.utils.TokenUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
